1c6f9c6c12ce83916cf614a8abb0ab61b2dac65e
ludoiphone <74968621+ludoiphone@users.noreply.github.com>
2025-07-20 21:42:02 +0200

    Add files via upload

---
+++ froid.cpp
@@ -0,0 +1,8 @@
+#include "froid.h"
+
+void Froid::refroidissement() 
+{
+    cout << "fabrication du refroidissement" << endl << endl;
+}
+
+
---
+++ froid.h
@@ -0,0 +1,14 @@
+#ifndef FROID_H
+#define FROID_H
+
+#include <iostream>
+using namespace std;
+
+class Froid
+{
+    
+public:
+    void refroidissement();
+};
+
+#endif //FROID_H
