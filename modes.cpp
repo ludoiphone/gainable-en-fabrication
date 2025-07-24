bf29661a8a10ae4035afaaf842c85af59dcec625
ludoiphone <74968621+ludoiphone@users.noreply.github.com>
2025-07-24 09:05:19 +0200

    modes.cpp

--- modes.cpp
+++ modes.cpp
@@ -15,12 +15,8 @@ void Modes::productions()
 void Modes::SetTemperatureExt(double temperatureExtLue)
 {
 	temperatureExt = temperatureExtLue;
-	
-	cout << "temperatureExterieur : " << temperatureExt << endl;
 }
 
 Modes::~Modes()
-{
-	cout << "destructeur Modes" << endl;
-	
+{	
 }
