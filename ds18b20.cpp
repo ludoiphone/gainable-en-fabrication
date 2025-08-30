8c3a1ae7a09103d5e13be0f58bbab37f63f7c284
ludoiphone <74968621+ludoiphone@users.noreply.github.com>
2025-08-30 16:54:10 +0200

    ds18b20.cpp

--- ds18b20.cpp
+++ ds18b20.cpp
@@ -1,55 +1,51 @@
 #include "ds18b20.h"
 
-float DS18B20::GetTempExt()
+float DS18B20::get_tempExt()
 {
-    FILE * temp = fopen("/sys/bus/w1/devices/28-3ce1e3804835/temperature","rt");
-    int numread = fread(buffer,10,1,temp);
-    fclose(temp);
+    FILE * probe1 = fopen("/sys/bus/w1/devices/w1_bus_master1/28-3ce1e3804835/temperature","rt");
+    int numread1 = fread(buffer,10,1,probe1);
+    fclose(probe1);
     tempExtLue = ("%5.2fC\n",strtol(buffer,&end,10)/1000.0);
-    
+
     return tempExtLue;
 }
 
-float DS18B20::GetTempUnitExt()
+float DS18B20::get_tempUnitExt()
 {
-    FILE * temp = fopen("/sys/bus/w1/devices/28-3ce1e3809744/temperature","rt");
-    int numread = fread(buffer,10,1,temp);
-    fclose(temp);
+    FILE * probe2 = fopen("/sys/bus/w1/devices/w1_bus_master1/28-3ce1e3809744/temperature","rt");
+    int numread2 = fread(buffer,10,1,probe2);
+    fclose(probe2);
     tempUnitExtLue = ("%5.2fC\n",strtol(buffer,&end,10)/1000.0);
     
     return tempUnitExtLue;
 }
 
-float DS18B20::GetTempEchExt()
+float DS18B20::get_tempEchExt()
 {
-    FILE * temp = fopen("/sys/bus/w1/devices/28-3ce1e38060ec/temperature","rt");
-    int numread = fread(buffer,10,1,temp);
-    fclose(temp);
+    FILE * probe3 = fopen("/sys/bus/w1/devices/w1_bus_master1/28-3ce1e38060ec/temperature","rt");
+    int numread3 = fread(buffer,10,1,probe3);
+    fclose(probe3);
     tempEchExtLue = ("%5.2fC\n",strtol(buffer,&end,10)/1000.0);
     
     return tempEchExtLue;
 }
 
-float DS18B20::GetTempUnitInt()
+float DS18B20::get_tempUnitInt()
 {
-    FILE * temp = fopen("/sys/bus/w1/devices/28-3ce1e3801251/temperature","rt");
-    int numread = fread(buffer,10,1,temp);
-    fclose(temp);
+    FILE * probe4 = fopen("/sys/bus/w1/devices/w1_bus_master1/28-3ce1e3801251/temperature","rt");
+    int numread4 = fread(buffer,10,1,probe4);
+    fclose(probe4);
     tempUnitIntLue = ("%5.2fC\n",strtol(buffer,&end,10)/1000.0);
     
     return tempUnitIntLue;
 }
 
-float DS18B20::GetTempEchInt()
+float DS18B20::get_tempEchInt()
 {
-    FILE * temp = fopen("/sys/bus/w1/devices/28-3ce1e3805e9f/temperature","rt");
-    int numread = fread(buffer,10,1,temp);
-    fclose(temp);
+    FILE * probe5 = fopen("/sys/bus/w1/devices/w1_bus_master1/28-3ce1e3805e9f/temperature","rt");
+    int numread5 = fread(buffer,10,1,probe5);
+    fclose(probe5);
     tempEchIntLue = ("%5.2fC\n",strtol(buffer,&end,10)/1000.0);
     
     return tempEchIntLue;
-}
-
-DS18B20::~DS18B20()
-{
 }
