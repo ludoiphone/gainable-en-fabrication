0ba287707ec7370690a5bf634175a16b05766424
ludoiphone <74968621+ludoiphone@users.noreply.github.com>
2025-12-02 06:03:50 +0100

    Froid.hpp

--- src/hardware/Froid.hpp
+++ src/hardware/Froid.hpp
@@ -2,7 +2,6 @@
 #include "hardware/Relay.hpp"
 #include "logic/Consignes.hpp"
 #include "logic/Temporisations.hpp"
-#include "utils/Logger.hpp"
 #include <chrono>
 
 enum class CoolingState {
@@ -12,9 +11,11 @@ enum class CoolingState {
     EGOUTTAGE
 };
 
+enum class VentSpeedCool { VENT_OFF = 0, VENT_LOW = 1, VENT_HIGH = 2 };/////
+
 class Froid {
 public:
-    Froid(Logger& logger, Consignes* consignes, Temporisations* tempos);
+    Froid(Consignes* consignes, Temporisations* tempos);
 
     void update(double tempUExt, double tempUInt, double tempEInt);
     void stop();
@@ -41,9 +42,6 @@ private:
     void setVentExt(bool on, bool grandeVitesse);
     void setVentInt(bool on, bool grandeVitesse);
 
-private:
-    Logger& m_logger;
-
     Relay m_compresseur;
     Relay m_ventExt;
     Relay m_vitVentExt;
@@ -60,4 +58,8 @@ private:
     std::chrono::steady_clock::time_point m_departTempoComp;
     std::chrono::steady_clock::time_point m_debutDegivrage;
     std::chrono::steady_clock::time_point m_debutEgouttage;
+    
+    // mémoire vitesse ventilateur extérieur
+    VentSpeedCool m_lastVentExtSpeed;
+    VentSpeedCool m_lastVentIntSpeed;
 };
